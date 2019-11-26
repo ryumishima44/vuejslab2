5fbcf20bf9717ab18c10062cf55685eea5450bb3
Francisco Sanchez <franciscos@criticalmass.com>
2019-11-26 15:59:57 -0600

    Platzi music basico sin nada

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,54 @@
 <template lang='pug'>
   #app
-    img(src='./assets/logo.png')
-    h1 {{ msg }}
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(type="text",
+                               placeholder="Buscar canciones",
+                               v-model='query')
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+
+        .container {{ searchMsg }}
+          .columns
+            .column(v-for="track in tracks") {{ track.name }} - {{ track.artist }}
 </template>
 <script>
+const tracks = [
+  {
+    name: 'Muchacha',
+    artist: 'Spindola'
+  },
+  {
+    name: 'Chaule',
+    artist: 'Prajakta'
+  },
+  {
+    name: 'Aguas wey',
+    artist: 'Tropikal forever'
+  }
+]
 export default {
-  name: 'app',
+  name: 'App',
+
   data () {
     return {
-      msg: 'Welcome to Plazi music'
+      query: '',
+      tracks: []
+    }
+  },
+  computed: {
+    searchMsg () {
+      return `${this.tracks.length} Resultados`
+    }
+  },
+  methods: {
+    search () {
+      this.tracks = tracks
     }
   }
 }
 </script>
 <style lang='scss'>
-@import './scss/main.scss'
+@import './scss/main.scss';
 </style>
